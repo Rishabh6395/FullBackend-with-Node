258e6b37d84cfa189c0352de4a9616ceb332a625
Rishabh <rishabhbhatt956@gmail.com>
2025-03-22 11:00:12 +0530

    Add examples of ESModules usage in Node.js with default, named, and aliased imports

---
+++ Day9/index.js
@@ -0,0 +1,17 @@
+// ESModules in Node.js: Default, Named, and Aliased Imports
+
+// refer this video for more info: https://www.youtube.com/watch?v=lY8lz_Pzm78&list=PLwGdqUZWnOp3KELplHtc-RnJ5xTUPqdgH&index=19
+
+// Default Imports
+// import { default as myModule } from "./myModule.js";
+
+// Named Imports
+// import { myFunction, myVariable } from "./myModule.js";
+
+// Aliased Imports
+// import { myFunction as myAlias } from "./myModule.js";
+
+// Exporting
+// export default myModule;
+// export { myFunction, myVariable };
+// export { myFunction as myAlias };
