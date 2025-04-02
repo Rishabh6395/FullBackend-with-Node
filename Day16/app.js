014f8b15c5f3791ce8f61d925b4d9465de4786ae
Rishabh <rishabhbhatt956@gmail.com>
2025-04-03 04:35:51 +0530

    Implement link loading and URL shortening functionality in Node.js server

--- Day16/app.js
+++ Day16/app.js
@@ -1,10 +1,12 @@
 // Serving HTML & CSS File in Node
 
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import { createServer } from 'http';
 import path from 'path';
+import crypto from 'crypto'
 
 const PORT = 3000;
+const DATA_FILE = path.join("data", 'links.json')
 
 const serveFile = async (res, filePath, contentType) => {
     try {
@@ -16,6 +18,19 @@ const serveFile = async (res, filePath, contentType) => {
         res.end("404 page not found"); // Send 404 error if file not found
     }
 };
+
+const loadLinks = async () => {
+    try {
+        const data = await readFile(DATA_FILE, 'utf-8')
+        return json.parse(data)
+    } catch (error) {
+        if(error.code === "ENOENT"){
+            await writeFile(DATA_FILE, JSON.stringify({}))
+            return {};
+        }
+        throw error;
+    }
+}
 
 const server = createServer(async (req, res) => {
     console.log(req.url);
@@ -35,6 +50,28 @@ const server = createServer(async (req, res) => {
         res.writeHead(405, { 'Content-type': 'text/plain' });
         res.end("Method not allowed");
     }
+
+    if(req.method === "POST" && req.url === "/shorten"){
+
+        const link = await loadLinks()
+
+        const body = ""
+        req.on("data", (chunk) => {
+            body = body + chunk;
+        })
+        req.on('end', () => {
+            console.log(body)
+            const {url, shortCode} = JSON.parse(body)
+
+            if(!url){
+                res.writeHead(400, {"Content-Type": "text/plain"})
+                return res.end("URL is required")
+            }
+            const findShortCode = shortCode || crypto.randomBytes(4).toString("hex")
+
+        })
+    }
+
 });
 
 server.listen(PORT, () => {
