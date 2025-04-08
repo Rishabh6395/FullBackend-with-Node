3169b86100c7c8a4247706a996be87522dd582c7
Rishabh <rishabhbhatt956@gmail.com>
2025-04-08 06:45:26 +0530

    Add global error handler and update dependencies for improved error management

--- MASTER REST APIs with Coders gyan/src/app.ts
+++ MASTER REST APIs with Coders gyan/src/app.ts
@@ -1,4 +1,6 @@
+
 import express from 'express'
+import globalErrorHandler from './middlewares/globalehandlers'
 
 const app = express()
 
@@ -6,8 +8,12 @@ const app = express()
 // HTTP Method: GET, POST, PUT, PATCH, DELETE
 
 app.get('/', (req, res, next) => {
+    
     res.json({message: "Welcome to elib APIs"})
 })
+
+app.use(globalErrorHandler);
+
 
 
 export default app;
---
+++ MASTER REST APIs with Coders gyan/src/middlewares/globalehandlers.ts
@@ -0,0 +1,21 @@
+// Global error handler
+
+import { Request, Response, NextFunction } from "express";
+import { HttpError } from "http-errors";
+import { config } from "../config/config";
+
+const globalErrorHandler = (
+    err: HttpError, 
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+): void => {
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        message: err.message,
+        errorStack: config.env === 'development' ? err.stack : '',
+    });
+};
+
+export default globalErrorHandler;
