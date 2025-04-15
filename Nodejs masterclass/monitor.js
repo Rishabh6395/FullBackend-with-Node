e627e71f6ddf32dd5b7b75eaadc62b0a44527bee
Rishabh <rishabhbhatt956@gmail.com>
2025-04-16 04:59:18 +0530

    Add CPU monitoring functionality: Implement periodic CPU usage snapshots and display in console (PENDING)

---
+++ Nodejs masterclass/monitor.js
@@ -0,0 +1,39 @@
+import os from 'node:os'
+
+
+function monitor(){
+    // Take a snapshot
+    // Take another snapshot after a second
+
+    const oldCPUs = os.cpus();
+
+    setTimeout(() => {
+        const newCPUs = os.cpus()
+        
+        const usage = newCPUs.map((cpu, i) => {
+            return {
+                core: i,
+                usage: calculateCPU(oldCPUs[i], newCPUs[i])+ '%',
+            }
+        })
+
+        console.clear()
+        console.table(usage)
+
+    },1000)
+}
+
+function calculateCPU(oldCPUs, newCPUs){
+    const oldTotal = Object.values(oldCPUs.times).reduce((a,b) => a+b)
+    const newTotal = Object.values(newCPUs.times).reduce((a,b) => a+b)
+
+    const idle = newCPUs.times.idle - oldCPUs.times.idle
+    const total = newTotal - oldTotal;
+
+    const used = total - idle;
+
+    return ((100 * used) / total).toFixed(1)
+}
+
+
+setInterval(monitor, 1000);
